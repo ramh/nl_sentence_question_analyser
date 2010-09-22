dff23258c8f5a1d60d1e7176937c4b43bfcf3caf
Ram Kumar Hariharan <ramk@gatech.edu>
2010-09-21 21:53:49 -0400

    Final Submission data

--- question_word_annotator.rb
+++ question_word_annotator.rb
@@ -1,13 +1,16 @@
+########################################################
+#### Question Word Annotator                        ####
+#### CS7650, Fall 2010                              ####
+#### Ram Kumar Hariharan (GTID: 902631808)          ####
+#### Blair Daly (GTID: 902268308)                   ####
+########################################################
+
 ENV["JAVA_HOME"] = "/usr/lib/jvm/java-6-openjdk/"
 ENV["CLASSPATH"] = "/home/ram/Data/gatech/NL/assignment1/stanford-parser-2010-08-20/stanford-parser-2010-08-20.jar"
 require "rubygems"
 require "stanfordparser"
 require "tree"
 require "ruby-debug"
-
-#RULES = { "ROOT-S-VP-VB" => "what",
-#          "ROOT-S-VP-PP-NP-NN" => "where",
-#          "ROOT-S-VP-NP-PRP" => "who" }
 
 # File for Storing Trained Model
 TRAINED_MODEL_FILE = "trained_model.txt"
@@ -22,11 +25,14 @@ def get_tree_object(stanford_parser_tree)
   root_node.children[0]
 end
 
+$random_number = 0
 def process_node(stanford_parser_tree_string, root_node)
   stanford_parser_tree_string.strip!
   if stanford_parser_tree_string =~ /^\([^\)]+\(/
     node_name = stanford_parser_tree_string.match(/^\(([A-Z]+)/)
-    child = Tree::TreeNode.new(node_name[1], "NON-LEAF")   #(root_node.content.to_i + 1).to_s)
+    $random_number+=1
+    random = ($random_number % 10).to_s
+    child = Tree::TreeNode.new(random + node_name[1], "NON-LEAF")   #(root_node.content.to_i + 1).to_s)
     root_node << child
     sub_string = stanford_parser_tree_string.match(/^\([^\)\(]+(\(.*\))\)$/)
     process_node(sub_string[1], child)
@@ -46,9 +52,11 @@ def process_node(stanford_parser_tree_string, root_node)
       current_index+=1
     end
     if sub_strings.length == 1
-      node_name = sub_strings[0].match(/^\(([A-Z]+)/)
-      node_content = sub_strings[0].match(/\]\s*([A-Za-z]+)/)
-      child = Tree::TreeNode.new(node_name[1], node_content[1])
+      node_name = sub_strings[0].match(/^\(([\.A-Z]+)/)
+      node_content = sub_strings[0].match(/\]\s*([\?'A-Za-z]+)/)
+      $random_number+=1
+      random = ($random_number % 10).to_s
+      child = Tree::TreeNode.new(random + node_name[1], node_content[1])
       root_node << child
     else
       for sub_string in sub_strings
@@ -70,6 +78,7 @@ def train(sentence)
   path_hash = {}
   array = []
   get_path_hash(parser_tree, path_hash, array)
+  p path_hash
 
   # Step 3: Open the existing trained data from file
   begin
@@ -101,13 +110,14 @@ def get_word_from_line(line)
 end
 
 def get_path_hash(parser_tree, path_hash, array)
+  name = parser_tree.name[1..-1] # remember we added random one letter string to avoid TreeNode uniqueness problem
   if parser_tree.content == "NON-LEAF"
-    array << parser_tree.name
+    array << name
     for child in parser_tree.children
       get_path_hash(child, path_hash, array.dup)
     end
   else
-    array << parser_tree.name
+    array << name
     path_hash[parser_tree.content] = array.join("-")
   end
 end
